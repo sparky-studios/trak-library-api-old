820a3a7c4d9fa31b20dc5337f1f65fc7082e2690
TheLordBritish <benjamin.carter.dev@gmail.com>
2020-04-27 10:55:08 +0100

    Ensure requests are logged for the auth-service. Added some JavaDoc.

--- auth-service/src/main/java/com/sparky/trak/authentication/service/dto/RegistrationRequestDto.java
+++ auth-service/src/main/java/com/sparky/trak/authentication/service/dto/RegistrationRequestDto.java
@@ -3,6 +3,7 @@ package com.sparky.trak.authentication.service.dto;
 import com.sparky.trak.authentication.service.validation.ValidPassword;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 @Data
@@ -11,7 +12,7 @@ public class RegistrationRequestDto {
     @NotEmpty(message = "{registration-request.validation.username.not-empty}")
     private String username;
 
-    @NotEmpty(message = "{registration-request.validation.email-address.not-empty}")
+    @Email(message = "{registration-request.validation.email-address.invalid}")
     private String emailAddress;
 
     @ValidPassword(message = "{registration-request.validation.password.invalid}")
